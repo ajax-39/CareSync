18b7f53c055315374ef58205bf3ebad94714eaf0
Atharv Jagzap <atharvjagzap@gmail.com>
2025-10-26 15:19:49 +0530

    Add Kafka producer

--- patient-service/src/main/java/com/pm/patientservice/grpc/BillingServiceGrpcClient.java
+++ patient-service/src/main/java/com/pm/patientservice/grpc/BillingServiceGrpcClient.java
@@ -12,27 +12,32 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class BillingServiceGrpcClient {
-    private static final Logger log = LoggerFactory.getLogger(BillingServiceGrpcClient.class);
+
+    private static final Logger log = LoggerFactory.getLogger(
+            BillingServiceGrpcClient.class);
     private final BillingServiceGrpc.BillingServiceBlockingStub blockingStub;
 
-    // localhost:90001/BillingService/CreatePatientAccount
-    //aws.grpc.123123/BillingService/CreatePatientAccount
+    public BillingServiceGrpcClient(
+            @Value("${billing.service.address:localhost}") String serverAddress,
+            @Value("${billing.service.grpc.port:9001}") int serverPort) {
 
-    public BillingServiceGrpcClient(@Value("${billing.service.address:localhost}") String serverAddress,
-                                    @Value("${biiling.service.grpc.port:9001") int serverPort) {
+        log.info("Connecting to Billing Service GRPC service at {}:{}",
+                serverAddress, serverPort);
 
-        log.info("Connecting to billing service GRPC at {} :{}", serverAddress, serverPort);
+        ManagedChannel channel = ManagedChannelBuilder.forAddress(serverAddress,
+                serverPort).usePlaintext().build();
 
-        ManagedChannel channel = ManagedChannelBuilder.forAddress(serverAddress, serverPort).usePlaintext().build();
         blockingStub = BillingServiceGrpc.newBlockingStub(channel);
-
     }
 
-    public BillingResponse createBillingAccount(String patientId,String name,String email){
-        BillingRequest request = BillingRequest.newBuilder().setPatientId(patientId).setName(name).setEmail(email).build();
+    public BillingResponse createBillingAccount(String patientId, String name,
+                                                String email) {
+
+        BillingRequest request = BillingRequest.newBuilder().setPatientId(patientId)
+                .setName(name).setEmail(email).build();
 
         BillingResponse response = blockingStub.createBillingAccount(request);
-        log.info("Create billing account response is {}", response);
+        log.info("Received response from billing service via GRPC: {}", response);
         return response;
     }
 }
---
+++ patient-service/src/main/java/com/pm/patientservice/kafka/KafkaProducer.java
@@ -0,0 +1,41 @@
+package com.pm.patientservice.kafka;
+
+import com.pm.patientservice.model.Patient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+import patient.events.PatientEvent;
+
+@Service
+public class KafkaProducer {
+
+    private static final Logger log = LoggerFactory.getLogger(
+            KafkaProducer.class);
+    private final KafkaTemplate<String, byte[]> kafkaTemplate;
+
+    public KafkaProducer(KafkaTemplate<String, byte[]> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendEvent(Patient patient) {
+        log.info("=== STARTING sendEvent for patient: {}", patient.getId());
+
+        PatientEvent event = PatientEvent.newBuilder()
+                .setPatientId(patient.getId().toString())
+                .setName(patient.getName())
+                .setEmail(patient.getEmail())
+                .setEventType("PATIENT_CREATED")
+                .build();
+
+        log.error("Created the event: {}", event);
+
+        try {
+            kafkaTemplate.send("patient", event.toByteArray());
+            log.info("=== Successfully sent event to Kafka");
+        } catch (Exception e) {
+            log.error("Error sending PatientCreated event: {}", event, e);
+        }
+    }
+
+}
